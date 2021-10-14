a75b976bcca9f586773ca8936c8b26a6db918648
JessPaley <jessicayelap@gmail.com>
2021-10-13 21:55:42 -0400

    started shortest path algorithm

--- DTW_matrix.py
+++ DTW_matrix.py
@@ -1,4 +1,6 @@
+import sys
 import numpy as np
+import heapq
 
 test_signal = np.array([1,4,5,10,9,3,2,6,8,4])
 test_signal2 = np.array([1,7,3,4,1,10,5,4,7,4])
@@ -31,4 +33,48 @@ def DTW_matrix(sig1, sig2):
     print(matrix)
     return matrix
 
-DTW_matrix(test_signal, test_signal2)
+# based on Dijkstra's Algorithm
+# starts at the first element in the matrix and ends at the last element
+# add visited nodes to new list and return list
+# # how to make it more efficient?
+def shortestPath(matrix):
+    edgeSet = makeEdges(matrix)
+    pq = []
+    output = []
+    heapq.heappush(pq, (0, (-1, -1), (0, 0)))  # start at first matrix element
+    while len(pq) > 0:
+        current = heapq.heappop(pq)
+        # to do:
+        # add connecting nodes to priority queue
+        # update distances in edgeSet
+        # add visited nodes and distances to output list
+
+    return output
+
+# helper method for shortestPath()
+# creates a tuple with distance initialized to the maximum integer, the
+# starting node (row, column), and the ending node (row, column)
+def makeEdges(matrix):
+    col = matrix.shape[0]
+    row = matrix.shape[1]
+    i = 0
+    j = 0
+    myEdges = []
+    while i < col:
+        while j < row:
+            if i < col - 1 and j < row - 1:
+                myEdges.append((sys.maxsize, (i, j), (i + 1, j + 1)))
+            if i < col - 1:
+                myEdges.append((sys.maxsize, (i, j), (i + 1, j)))
+            if j < row - 1:
+                myEdges.append((sys.maxsize, (i, j), (i, j + 1)))
+            j += 1
+        j = 0
+        i += 1
+    return myEdges
+
+
+test1 = np.array([1, 4, 2])
+test2 = np.array([1, 7, 3])
+
+shortestPath(DTW_matrix(test1, test2))
