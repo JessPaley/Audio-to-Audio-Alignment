1d727ee364ae84827223a4edb6f9d789b946925b
tangy1227 <ytang363@gatech.edu>
2021-10-19 01:32:12 -0400

    Updated DTW Calculation
    Separate distance matrix with cost matrix. Shortest path calculated.

--- DTW_matrix.py
+++ DTW_matrix.py
@@ -1,80 +1,70 @@
-import sys
 import numpy as np
-import heapq
 
-test_signal = np.array([1,4,5,10,9,3,2,6,8,4])
-test_signal2 = np.array([1,7,3,4,1,10,5,4,7,4])
+# test_signal = np.array([0,4,4,0,-4,-4,0]) # y-axis
+# test_signal2 = np.array([1,3,4,3,1,-1,-2,-1,0]) # x-axis
 
-def DTW_matrix(sig1, sig2):
-    matrix = np.zeros((sig1.size, sig2.size))
-    column = matrix.shape[0]
-    row = matrix.shape[1]
+test_signal = np.array([1,6,8,2]) # y-axis
+test_signal2 = np.array([4,2,5,7]) # x-axis
 
-    # sig1[i] index through column, sig2[j] index through row in each column
-    for i in range(column): 
+### Distance Matrix ###
+def Distance_matrix(sig1, sig2):
+    dis_matrix = np.zeros((sig1.size, sig2.size))
+    column = dis_matrix.shape[0]
+    row = dis_matrix.shape[1]
+    for i in range(column):
         for j in range(row):
-            value = np.abs(sig1[i]-sig2[j])
-            if i == 0 and j == 0:
-                # print("i=", i, "& j=", j, "t1")
-                minvalue = 0
-                matrix[i,j] = value + minvalue
-            elif i == 0:
-                # print("i=", i, "& j=", j, "t2")
-                minvalue = matrix[i, j-1]
-                matrix[i,j] = value + minvalue
-            elif j == 0:
-                # print("i=", i, "& j=", j, 't3')
-                minvalue = matrix[i-1, j]
-                matrix[i,j] = value + minvalue
-            else:
-                # print("i=", i, "& j=", j, 't4')
-                minvalue = min(matrix[i-1,j-1], matrix[i,j-1], matrix[i-1,j])
-                matrix[i,j] = value + minvalue
+            dis_matrix[i,j] = np.abs(sig1[i]-sig2[j])
+    return dis_matrix
+
+### Cost Matrix ###
+def Cost_matrix(sig1, sig2):
+    d_matrix = Distance_matrix(sig1, sig2)
+    c_matrix = np.zeros((sig1.size, sig2.size))
+    column = c_matrix.shape[0]
+    row = c_matrix.shape[1]
+
+    # c_matrix[0, 1:] = Calculate First Row
+    n = 0
+    for i in range(0, sig2.size):
+        n = d_matrix[0,i] + n
+        c_matrix[0, i] = n
+    
+    # c_matrix[1:, 0] = Calculate First Column
+    n = 0
+    for i in range(0, sig1.size):
+        n = d_matrix[i, 0] + n
+        c_matrix[i, 0] = n
+    
+    for i in range(1, column):
+        for j in range(1, row):
+            minvalue = min(c_matrix[i-1,j-1], c_matrix[i,j-1], c_matrix[i-1,j])
+            c_matrix[i,j] = d_matrix[i, j] + minvalue
+    # print(d_matrix)
+    # print(c_matrix)
+    return d_matrix, c_matrix
+Cost_matrix(test_signal, test_signal2)
+
+### DTW Path ###
+def DTW(matrix):
+    i = matrix.shape[0] - 1
+    j = matrix.shape[1] - 1
+    path = [(i + 1,j + 1)]
+    while i > 0 or j > 0:
+        a_list = [matrix[i-1,j-1], matrix[i,j-1], matrix[i-1,j]]
+        minvalue = min(a_list)
+        min_index = a_list.index(minvalue)
+        if min_index == 0:
+            i = i - 1
+            j = j - 1
+        elif min_index == 1:
+            i = i
+            j = j - 1
+        elif min_index == 2:
+            i = i - 1
+            j = j
+        path.append((i+1,j+1))
     print(matrix)
-    return matrix
-
-# based on Dijkstra's Algorithm
-# starts at the first element in the matrix and ends at the last element
-# add visited nodes to new list and return list
-# # how to make it more efficient?
-def shortestPath(matrix):
-    edgeSet = makeEdges(matrix)
-    pq = []
-    output = []
-    heapq.heappush(pq, (0, (-1, -1), (0, 0)))  # start at first matrix element
-    while len(pq) > 0:
-        current = heapq.heappop(pq)
-        # to do:
-        # add connecting nodes to priority queue
-        # update distances in edgeSet
-        # add visited nodes and distances to output list
-
-    return output
-
-# helper method for shortestPath()
-# creates a tuple with distance initialized to the maximum integer, the
-# starting node (row, column), and the ending node (row, column)
-def makeEdges(matrix):
-    col = matrix.shape[0]
-    row = matrix.shape[1]
-    i = 0
-    j = 0
-    myEdges = []
-    while i < col:
-        while j < row:
-            if i < col - 1 and j < row - 1:
-                myEdges.append((sys.maxsize, (i, j), (i + 1, j + 1)))
-            if i < col - 1:
-                myEdges.append((sys.maxsize, (i, j), (i + 1, j)))
-            if j < row - 1:
-                myEdges.append((sys.maxsize, (i, j), (i, j + 1)))
-            j += 1
-        j = 0
-        i += 1
-    return myEdges
-
-
-test1 = np.array([1, 4, 2])
-test2 = np.array([1, 7, 3])
-
-shortestPath(DTW_matrix(test1, test2))
+    print(path)
+    return path
+c_matrix = Cost_matrix(test_signal, test_signal2)[1]
+DTW(c_matrix) # Return list of tuples, (y-axis, x-axis)
