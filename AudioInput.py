eff0a98a60b98fbd8211be66c159f6e04304420b
tangy1227 <ytang363@gatech.edu>
2021-10-19 01:40:12 -0400

    Block Audio
    Dr. Lerch's code for read audio and blocking audio

---
+++ AudioInput.py
@@ -0,0 +1,36 @@
+import numpy as np
+from scipy.io.wavfile import read as wavread
+import math
+
+### Block Audio ###
+def block_audio(x,blockSize,hopSize,fs):
+    # allocate memory
+    numBlocks = math.ceil(x.size / hopSize)
+    xb = np.zeros([numBlocks, blockSize])
+    # compute time stamps
+    t = (np.arange(0, numBlocks) * hopSize) / fs
+    x = np.concatenate((x, np.zeros(blockSize)),axis=0)
+    for n in range(0, numBlocks):
+        i_start = n * hopSize
+        i_stop = np.min([x.size - 1, i_start + blockSize - 1])
+        xb[n][np.arange(0,blockSize)] = x[np.arange(i_start, i_stop + 1)]
+    return (xb,t)
+
+### Read Audio ###
+def ToolReadAudio(cAudioFilePath):
+    [samplerate, x] = wavread(cAudioFilePath)
+    if x.dtype == 'float32':
+        audio = x
+    else:
+        # change range to [-1,1)
+        if x.dtype == 'uint8':
+            nbits = 8
+        elif x.dtype == 'int16':
+            nbits = 16
+        elif x.dtype == 'int32':
+            nbits = 32
+        audio = x / float(2**(nbits - 1))
+    # special case of unsigned format
+    if x.dtype == 'uint8':
+        audio = audio - 1.
+    return(samplerate, audio)
