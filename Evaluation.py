ef41d8e7b8adc04cea0fb5d64ba516b2f08821b2
JessPaley <jessicayelap@gmail.com>
2021-11-16 12:22:21 -0500

    update evaluation

---
+++ Evaluation.py
@@ -0,0 +1,7 @@
+# Compare the timings found by our algorithm with the existing beat onset data
+
+# search the first row of the corresponding csv file to find the right take (same name as the audio file)
+# for each beat onset row in that column compare the time noted in the csv file with the time found by our algorithm
+# compute the difference between these two values to find the accuracy of our algorithm?
+# note any outliers in our data to identify places to improve
+
