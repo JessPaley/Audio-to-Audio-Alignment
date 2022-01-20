5389fdd0cb02a37ad629aeb3c70c069bf013a661
tangy1227 <ytang363@gatech.edu>
2022-01-20 05:06:22 -0500

    Subsequence Search Using Lowest Cost Path

--- subsequence.py
+++ subsequence.py
@@ -1,13 +1,57 @@
+import librosa, librosa.display
 import numpy as np
-from numpy.lib import row_stack
 from scipy.io.wavfile import read as wavread
 from scipy.spatial.distance import pdist, cdist
+import matplotlib.pyplot as plt
 
 # To do:
 # DTW determines multiple possible path candidates representing the potentially overlapping between pair of recording
+# Start Running DTW column by column? From the Cost Matrix?
 
 # Needs to consider all possible subsequences of Y to find the optimal one
 # - Determining the cost of an optimal warping path
+
+### Read Audio ###
+def ToolReadAudio(cAudioFilePath):
+    [samplerate, x] = wavread(cAudioFilePath)
+    if x.dtype == 'float32':
+        audio = x
+    else:
+        # change range to [-1,1)
+        if x.dtype == 'uint8':
+            nbits = 8
+        elif x.dtype == 'int16':
+            nbits = 16
+        elif x.dtype == 'int32':
+            nbits = 32
+        audio = x / float(2**(nbits - 1))
+    # special case of unsigned format
+    if x.dtype == 'uint8':
+        audio = audio - 1.
+    
+    # x, sr = librosa.load(cAudioFilePath)
+    # samplerate = sr
+    # audio = x
+    return(samplerate, audio)
+
+### Pitch Chroma ###
+def chroma(audio, sr=44100, blockSize=2048, hopSize=512):
+    S = np.abs(librosa.stft(y=audio, n_fft=blockSize, hop_length=hopSize))
+    chromagram = librosa.feature.chroma_stft(S=S, sr=sr, n_fft=blockSize, hop_length=hopSize, n_chroma=12)
+    t_chromagram = chromagram.T
+    # norm_chromagram = t_chromagram
+
+    # normalize the chromagram, sum up to 1
+    norm_chromagram = np.zeros([t_chromagram.shape[0],t_chromagram.shape[1]])
+    for i in range(t_chromagram.shape[0]):
+        norm_chromagram[i] = t_chromagram[i] / np.sum(t_chromagram[i])
+    return norm_chromagram
+
+### Feature Vector ###
+def featVector(path):
+    fs, audio = ToolReadAudio(path)
+    chromagram = chroma(audio, sr=fs)
+    return chromagram
 
 ### Calculate Euclidean Distance ###
 def Distance_matrix(sig1, sig2):
@@ -19,9 +63,11 @@ def Distance_matrix(sig1, sig2):
     #     for j in range(row):
     #         dis_matrix[i,j] = np.abs(sig1[i]-sig2[j])
     
-    # Using cdist
-    X,Y = np.atleast_2d(sig1, sig2)
-    dis_matrix = cdist(X.T, Y.T,'euclidean')
+    # # Using cdist
+    # X,Y = np.atleast_2d(sig1, sig2)
+    # dis_matrix = cdist(X.T, Y.T,'euclidean')
+
+    dis_matrix = cdist(sig1, sig2,'euclidean')
     return dis_matrix
 
 ### Cost Matrix ###
@@ -48,39 +94,97 @@ def Cost_matrix(sig1, sig2):
     return c_matrix
 
 ### Calculate DTW Path ###
-def DTW(matrix):
-    i = matrix.shape[0] - 1
-    j = matrix.shape[1] - 1
-    # path = [[i + 1,j + 1]]
-    path = [[i, j]]
-    while i > 0 or j > 0:
-        a_list = [matrix[i-1,j-1], matrix[i,j-1], matrix[i-1,j]]
-        minvalue = min(a_list)
-        min_index = a_list.index(minvalue)
-        if min_index == 0:
-            i = i - 1
-            j = j - 1
-        elif min_index == 1:
-            i = i
-            j = j - 1
-        elif min_index == 2:
-            i = i - 1
-            j = j
-        # path.append([i+1,j+1])
-        path.append([i,j])
-    path_np = np.flip(np.array(path))
-    # print(matrix)
-    # print(path)
+def modified_DTW(matrix, runAll=True):
+    N = matrix.shape[0] # Row
+    M = matrix.shape[1] # Column
+    if runAll==True:
+        for a in range(0, M):
+            n = N - 1
+            m = a
+            path = [[n, m]]
+            while n > 0:
+                if m == 0:
+                    n = n-1
+                    m = 0
+                else:
+                    a_list = [matrix[n-1,m-1], matrix[n,m-1], matrix[n-1,m]]
+                    minvalue = min(a_list)
+                    min_index = a_list.index(minvalue)
+                    if min_index == 0:
+                        n = n - 1
+                        m = m - 1
+                    elif min_index == 1:
+                        n = n
+                        m = m - 1
+                    elif min_index == 2:
+                        n = n - 1
+                        m = m
+                path.append([n,m])
+                path.reverse()
+            # path_np = np.flip(np.array(path))
+            path_np = np.array(path)
+            print(path_np)
+
+    elif runAll==False:
+        n = N - 1
+        m = matrix[-1, :].argmin() # Locate the lowest cost index from distance matrix
+        path = [[n, m]]
+        while n > 0:
+            if m == 0:
+                n = n-1
+                m = 0
+            else:
+                a_list = [matrix[n-1,m-1], matrix[n,m-1], matrix[n-1,m]]
+                minvalue = min(a_list)
+                min_index = a_list.index(minvalue)
+                if min_index == 0:
+                    n = n - 1
+                    m = m - 1
+                elif min_index == 1:
+                    n = n
+                    m = m - 1
+                elif min_index == 2:
+                    n = n - 1
+                    m = m
+            path.append([n,m])
+            path.reverse()
+        # path_np = np.flip(np.array(path))
+        path_np = np.array(path)
+        print(path_np)
+
     return path_np
 
+# # Number Test:
+# X = np.array([3, 0, 6])
+# Y = np.array([2, 4, 0, 4, 0, 0, 5, 2])
+# # X = np.array([1, 3, 9, 2, 1])
+# # Y = np.array([2, 0, 0, 8, 7, 2])
+# d_matrix = Distance_matrix(X,Y)
+# c_matrix = Cost_matrix(X,Y) 
+# path = modified_DTW(c_matrix, runAll=False)
 
-X = np.array([3, 0, 6])
-Y = np.array([2, 4, 0, 4, 0, 0, 5, 2])
 
-d_matrix = Distance_matrix(X,Y)
-c_matrix = Cost_matrix(X,Y) 
-path = DTW(c_matrix)
-# print(d_matrix)
-print(c_matrix)
-print(c_matrix[-1,:])
-print(path)
+# Audio Test:
+file = 'Assignments/7100 Research (Local File)/Subsequence(pid9048-01_bip).wav'
+file2 = "Assignments/7100 Research (Local File)/Full(pid1263-01_bip).wav" #ref
+chromaVec = featVector(file)
+chromaVec2 = featVector(file2) #ref
+
+d_matrix = Distance_matrix(chromaVec,chromaVec2)
+c_matrix = Cost_matrix(chromaVec,chromaVec2) 
+path = modified_DTW(c_matrix, runAll=False)
+# print(path)
+
+plt.imshow(d_matrix, origin='lower', aspect='auto')
+plt.plot(path[:, 1], path[:, 0], color='r')
+plt.clim([0, np.max(d_matrix)])
+plt.colorbar()
+plt.title('Distance Matrix - Pitch Chroma')
+plt.xlabel('Sequence Reference')
+plt.ylabel('Sequence X')
+
+plt.tight_layout()
+plt.show()
+
+
+
