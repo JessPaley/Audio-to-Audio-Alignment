c71cfde2c1a3fcb61b302fea5a36d982903309a9
JessPaley <jessicayelap@gmail.com>
2022-01-27 12:49:08 -0500

    Update Reaper.py

--- Reaper.py
+++ Reaper.py
@@ -1,11 +1,23 @@
 from reaper_python import *
 
 def main():
-    RPR_InsertTrackAtIndex(0,1)
+    items = RPR_CountMediaItems(0)
+    track1 = RPR_GetTrack(0, 0)
+    track2 = RPR_GetTrack(0, 1)
+    clip1 = RPR_GetMediaItem(0, 0)
+    clip2 = RPR_GetMediaItem(0, 1)
+    # use DTW
+    testArray = [1, 2, 3] # beat alignment from DTW
+    for i in testArray:
+      RPR_SetTakeStretchMarker(clip2, i, 0, 0)
+    
+
+   
 
 main()
 
-# RPR_setTakeStretchMarker()
-# two tracks - aligned
+# move start of subsequence clip to correct location
+# get item by take?
 # use stretch markers for each beat onset datum
 # align to first track
+# handle multiple items on a track?
