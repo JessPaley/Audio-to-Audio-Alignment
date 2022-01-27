17ff3b1e8b9d279409f363fc55e2a710d2ae633c
JessPaley <jessicayelap@gmail.com>
2022-01-27 10:04:48 -0500

    Initial Commit

---
+++ Reaper.py
@@ -0,0 +1,11 @@
+from reaper_python import *
+
+def main():
+    RPR_InsertTrackAtIndex(0,1)
+
+main()
+
+# RPR_setTakeStretchMarker()
+# two tracks - aligned
+# use stretch markers for each beat onset datum
+# align to first track
