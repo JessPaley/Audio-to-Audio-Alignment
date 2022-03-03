2cc582c77d9756d6d748396a32a6de531320c36d
JessPaley <jessicayelap@gmail.com>
2022-03-03 10:48:53 -0500

    Update FileDirectory.py

--- Audio Alignment Project/FileDirectory.py
+++ Audio Alignment Project/FileDirectory.py
@@ -2,5 +2,7 @@ ReferenceAudio = "7100 Research (Local File)/mazurka06-1/pid9072-01.wav"
 AudioSnippet = "7100 Research (Local File)/Old Test Audio/pid9192 snippet.wav"
 AudioFolder = "Audio Snippet Folder"
 
+ReferenceAudio = "../pid9072-01.wav"
+AudioSnippet = "../pid9063-01.wav"
 # Let user input the folder directory.
 # The folder should contains all the audio snippets
